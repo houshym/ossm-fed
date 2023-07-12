ddf5592b1fd81dfb10728f970bdbb6abb33d55df
MHS <msarvest@redhat.com>
2023-07-12 13:09:17 -0700

    optimize code

--- clean.sh
+++ clean.sh
@@ -1,17 +1,18 @@
-oc config use-context rosa
-oc delete project rosa-prod-mesh
-oc delete project prod-bookinfo
-oc delete -f ./ossm-operator/ossm.yaml
+#!/bin/bash
 
-oc config use-context aro 
-oc delete project aro-stg-mesh
-oc delete project stg-bookinfo
-oc delete -f ./ossm-operator/ossm.yaml
+function delete_federation() {
+    local context=$1
+    local project_prefix=$2
 
-oc config use-context rog
-oc delete project gcp-dev-mesh
-oc delete project dev-bookinfo
-oc delete -f ./ossm-operator/ossm.yaml
+    oc config use-context $context
+    oc delete project $project_prefix-mesh
+    oc delete project $project_prefix-bookinfo
+    oc delete -f ./ossm-operator/ossm.yaml
+}
 
-echo "if you want to delete the operators run the following command"
+delete_federation "rosa" "rosa-prod"
+delete_federation "aro" "aro-stg"
+delete_federation "rog" "gcp-dev"
+
+echo "If you want to delete the operators, run the following command:"
 echo "oc delete csv <csv-name>"
